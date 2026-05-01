7e2ffb851175d7d0699543cb7d7ecb5d886fafcf
Damian Seals <46690930+dseals3@users.noreply.github.com>
2026-05-01 00:17:58 -0400

    created basic work data

---
+++ app/data/work.ts
@@ -0,0 +1,17 @@
+export const work = [
+  {
+    title: "VCReversal",
+    type: "short film",
+    description: "Director, Director of Photography, Editor, Colorist",
+  },
+  {
+    title: "for those who feel like giving up...",
+    type: "youtube video",
+    description: "I did everything",
+  },
+  {
+    title: "This Website",
+    type: "code",
+    description: "Built with Next.js as a Film x Code portfolio",
+  },
+];
