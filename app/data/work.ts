c41781543e67bdf9ced662a7ceee761d29526f7b
Damian Seals <46690930+dseals3@users.noreply.github.com>
2026-05-01 01:54:38 -0400

    added thumbnail & video to data

--- app/data/work.ts
+++ app/data/work.ts
@@ -2,16 +2,25 @@ export const work = [
   {
     title: "VCReversal",
     type: "short film",
+    year: "2026",
+    thumbnail: "",
+    video: "/video/aurora-timelapse-2.mp4",
     description: "Director, Director of Photography, Editor, Colorist",
   },
   {
     title: "for those who feel like giving up...",
     type: "youtube video",
+    year: "2025",
+    thumbnail: "",
+    video: "",
     description: "I did everything",
   },
   {
     title: "This Website",
     type: "code",
+    year: "2026",
+    thumbnail: "",
+    video: "",
     description: "Built with Next.js as a Film x Code portfolio",
   },
 ];
