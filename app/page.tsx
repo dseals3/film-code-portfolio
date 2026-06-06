cda8844a57b7e733ca7644a89fe8e4cbf586f52f
Damian Seals <46690930+dseals3@users.noreply.github.com>
2026-06-06 19:46:09 -0400

    made max grid width 2 cols

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
       <section id="selected-work" className="py-24 px-6 max-w-5xl mx-auto">
         <h2 className="text-2xl font-semibold mb-8">Selected Work</h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 max-w-6xl mx-auto gap-6 ">
           {work
             .filter((item) => item.featured)
             .map((item) => (
