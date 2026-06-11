cac6ad14918abf78ccbc64b6313e1b268d896611
Damian Seals <46690930+dseals3@users.noreply.github.com>
2026-06-10 20:37:32 -0400

    added btsImages and paths to data structure

--- app/data/work.ts
+++ app/data/work.ts
@@ -4,13 +4,17 @@ export const work = [
     title: "for those who feel like giving up...",
     type: "youtube video",
     year: "2025",
-    thumbnail: "/images/dontgiveup_thumbnail_720.png",
+    thumbnail: "/images/dontgiveup/dontgiveup_thumbnail_720.png",
     previewVideo: "/video/dontgiveup-prv.mp4",
     fullVideo: "https://www.youtube.com/embed/h6nf7L9Wvp0",
     roles: ["Writer", "Director", "Director of Photography", "Gaffer", "Editor", "Colorist"],
     authorship: "Created Entirely by Damian Seals",
     description: "After taking a gap year to pursue my life's goal of becoming a youtuber and not seeing any progress, I share the 3 most important lessons I'd learned in that gap year.",
     tools: ["Sony a7iv", "Sony ZV-E1", "DJI Mini 3 Pro", "Custom Drop Rig", "Lights", "Stands", "Hazer", "Davinci Resolve",],
+    btsImages: [
+      "/images/dontgiveup/240.jpg",
+      "/images/dontgiveup/cam-blue.jpg"
+    ],
     featured: true
   },
   {
@@ -18,12 +22,18 @@ export const work = [
     title: "I Need a Hero: Life. Pass it on.",
     type: "commercial",
     year: "2026",
-    thumbnail: "/images/liveonNY.jpg",
+    thumbnail: "/images/liveonNY/liveonNY.jpg",
     previewVideo: "/video/liveonny-prv.mp4",
     fullVideo: "https://www.youtube.com/embed/tNKRdvi7OiI",
     roles: ["Grip"],
     description: "Set within a hospital, LiveOnNY's public service campaign centers on a patient in need of a life-saving organ, as family members and loved ones deliver a somber and deeply moving rendition of the 1980s hit song, “Holding Out for a Hero,” by Bonnie Tyler. LiveOnNY.org",
     tools: ["Lights", "C-Stands"],
+    btsImages: [
+      "/images/liveonNY/ny-bts-1.jpg",
+      "/images/liveonNY/ny-bts-2.jpg",
+      "/images/liveonNY/ny-bts-3.jpg",
+      "/images/liveonNY/ny-bts-4.jpg"
+    ],
     featured: true
   },
   {
@@ -31,12 +41,22 @@ export const work = [
     title: "Kpreme - Pink Lemonade [Official Music Video]",
     type: "music video",
     year: "2026",
-    thumbnail: "/images/kpreme-thnail.jpg",
+    thumbnail: "/images/pink-lemonade/kpreme-thnail.jpg",
     previewVideo: "/video/kpreme-prv.mp4",
     fullVideo: "https://www.youtube.com/embed/IjDnBJbnPTM",
     roles: ["1st Assistant Camera"],
     description: "Official Music Video for Pink Lemonade performed by Kpreme",
     tools: ["Blackmagic Pocket 6k Pro", "Plexiglass Platform"],
+    btsImages: [
+      "/images/pink-lemonade/pl-bts-1.jpg",
+      "/images/pink-lemonade/pl-bts-2.jpg",
+      "/images/pink-lemonade/pl-bts-3.jpg",
+      "/images/pink-lemonade/pl-bts-4.jpg",
+      "/images/pink-lemonade/pl-bts-5.jpg",
+      "/images/pink-lemonade/pl-bts-6.jpg",
+      "/images/pink-lemonade/pl-bts-7.jpg",
+
+    ],
     featured: true
   },
   {
@@ -44,12 +64,17 @@ export const work = [
     title: "Dear David | Knoxville 7 Day Shootout Film Competition",
     type: "short film",
     year: "2025",
-    thumbnail: "/images/dear-david-thnail.jpg",
+    thumbnail: "/images/dear-david/dear-david-thnail.jpg",
     previewVideo: "/video/dear-david-prv.mp4",
     fullVideo: "https://www.youtube.com/embed/XZF__F6svTU",
     roles: ["Director of Photography", "Colorist"],
     description: "Months after the death of his wife, David prepares to move on. But when a discovery among her belongings reveals an unexpected chapter of her past, he is forced to confront the complicated legacy she left behind.\n\nCreated as part of the Knoxville 7-Day Shootout film challenge, this character-driven drama explores grief, memory, and the search for closure.\n\nThe film was recognized with nominations across multiple categories, including Best Film, Directing, Cinematography, Writing, Sound Design, Use of Music, and Best Actor in a Leading Role. It also earned 3rd Place in the Audience Favorite category, while its director received the Emerging Filmmaker Award.",
     tools: ["Sony A7iv", "Sony FX30", "Amaran 300c", "C-Stand", "Davinci Resolve"],
+    btsImages: [
+      "/images/dear-david/dd-bts-1.jpg",
+      "/images/dear-david/dd-bts-2.jpg",
+      "/images/dear-david/dd-bts-3.jpg",
+    ],
     featured: true
   },
   {
@@ -63,6 +88,7 @@ export const work = [
     roles: [""],
     description: "A custom-built portfolio created to showcase my work in filmmaking and visual storytelling. Built from the ground up using Next.js, TypeScript, and Tailwind CSS, the site was designed with an emphasis on performance, intuitive navigation, and a viewing experience tailored to creative work.",
     tools: ["Next.js", "TypeScript", "Tailwind CSS", "Git", "Vercel"],
+    btsImages: [],
     featured: true
   },
   {
@@ -77,6 +103,7 @@ export const work = [
     roles: ["Writer", "Director", "Director of Photography", "Gaffer", "Editor", "Colorist"],
     description: "",
     tools: ["Sony A7iv", "GoPro", "Lights", "C-Stand", "Davinci Resolve"],
+    btsImages: [],
     featured: false
   },
 ];
