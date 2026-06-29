7c10b601c7eaeb2f747502439823564d720b9885
Damian Seals <46690930+dseals3@users.noreply.github.com>
2026-06-28 22:18:10 -0400

    styled imageviewer to make black images on black bg's have more separation

--- app/components/ImageViewer.tsx
+++ app/components/ImageViewer.tsx
@@ -14,13 +14,20 @@ export default function ImageViewer({
 }: Props) {
     return (
         <figure className="my-8">
-            <div className="overflow-hidden rounded-xl border border-white/10">
+            <div className="
+                w-full max-w-5xl mx-auto mb-8
+                p-2
+                bg-zinc-800/80
+                border border-white/20
+                rounded-xl
+                overflow-hidden
+                shadow-[0_0_20px_rgba(255,255,255,0.35),0_0_60px_rgba(255,255,255,0.15)]">
                 <Image
                     src={src}
                     alt={alt}
                     width={1600}
                     height={900}
-                    className="w-full h-auto"
+                    className="w-full h-auto rounded-lg border border-white/5"
                     priority
                 />
             </div>
