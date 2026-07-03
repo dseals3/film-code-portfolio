feb28db1288b7e8cf2a1cbb6e3114f61f85cc202
Damian Seals <46690930+dseals3@users.noreply.github.com>
2026-07-02 23:08:53 -0400

    made contact btn a comp - didn't want entire homepage to be on client, but needed it for this

---
+++ app/components/ScrolltoContactBtn.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+export function ScrollToContactBtn() {
+  return (
+    <button
+      onClick={() => {
+        document.getElementById("contact")?.scrollIntoView({
+          behavior: "smooth",
+        });
+      }}
+      className="px-6 py-3 border border-white rounded-full transition ease-in-out hover:scale-102 hover:bg-white/10 hover:backdrop-blur-sm"
+    >
+      Contact
+    </button>
+  );
+}
