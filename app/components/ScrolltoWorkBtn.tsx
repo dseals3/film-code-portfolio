412a92a5b04fd55f6c9dd5998ea0932b4627e894
Damian Seals <46690930+dseals3@users.noreply.github.com>
2026-07-03 18:59:41 -0400

    Fixed issues & made cursor pointer on hover

--- app/components/ScrolltoWorkBtn.tsx
+++ app/components/ScrolltoWorkBtn.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export function ScrollToContactBtn() {
+export function ScrollToWorkBtn() {
   return (
     <button
       onClick={() => {
@@ -8,7 +8,7 @@ export function ScrollToContactBtn() {
           behavior: "smooth",
         });
       }}
-      className="px-6 py-3 bg-white text-black rounded-full transition ease-in-out hover:scale-105 hover:bg-zinc-200"
+      className="px-6 py-3 bg-white text-black rounded-full transition ease-in-out cursor-pointer hover:scale-105 hover:bg-zinc-200"
     >
       View Work
     </button>
