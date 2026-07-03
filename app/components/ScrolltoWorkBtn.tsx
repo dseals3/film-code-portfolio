249f6df325de5ab93fbe8b0e16e57d48ff1e4f7a
Damian Seals <46690930+dseals3@users.noreply.github.com>
2026-07-03 18:55:51 -0400

    changed styling to match existing btn

--- app/components/ScrolltoWorkBtn.tsx
+++ app/components/ScrolltoWorkBtn.tsx
@@ -4,11 +4,11 @@ export function ScrollToContactBtn() {
   return (
     <button
       onClick={() => {
-        document.getElementById("contact")?.scrollIntoView({
+        document.getElementById("selected-work")?.scrollIntoView({
           behavior: "smooth",
         });
       }}
-      className="px-6 py-3 border border-white rounded-full transition ease-in-out cursor-pointer hover:scale-102 hover:bg-white/10 hover:backdrop-blur-sm"
+      className="px-6 py-3 bg-white text-black rounded-full transition ease-in-out hover:scale-105 hover:bg-zinc-200"
     >
       View Work
     </button>
