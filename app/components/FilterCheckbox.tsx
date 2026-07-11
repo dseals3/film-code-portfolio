ac43656129b636559bd9637664b9bc0bd47c1593
Damian Seals <46690930+dseals3@users.noreply.github.com>
2026-07-11 11:28:46 -0400

    made filter checkbox into component

---
+++ app/components/FilterCheckbox.tsx
@@ -0,0 +1,41 @@
+type FilterCheckboxProps = {
+    label: string;
+};
+
+export default function FilterCheckbox({
+    label,
+}: FilterCheckboxProps) {
+    return (
+        <label
+            className="
+                flex items-center gap-3
+                py-2
+                text-md
+                text-gray-300
+                cursor-pointer
+            ">
+            <input
+                type="checkbox"
+                className="peer sr-only"
+            />
+
+            <div
+                className="
+                h-4 w-4
+                rounded
+                border border-white/20
+                bg-white/[0.02]
+
+                peer-checked:bg-white
+                peer-checked:border-white
+
+                border border-white/20
+                    hover:border-white/40
+
+                transition
+                "
+            />
+            {label}
+        </label>
+    );
+}
