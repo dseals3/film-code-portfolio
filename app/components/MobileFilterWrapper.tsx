469a1102860aa2ae84e99a204bc201826ccb807b
Damian Seals <46690930+dseals3@users.noreply.github.com>
2026-07-18 12:54:42 -0400

    mobile filters btn looks much better

--- app/components/MobileFilterWrapper.tsx
+++ app/components/MobileFilterWrapper.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { ListFilter } from "lucide-react";
 
 export default function MobileFilterWrapper({
   children,
@@ -13,9 +14,35 @@ export default function MobileFilterWrapper({
     <>
       <button
         onClick={() => setIsOpen(!isOpen)}
-        className="lg:hidden"
+        className="
+            lg:hidden
+            
+            w-full
+
+            flex
+            items-center
+            justify-between
+
+            rounded-xl
+            border border-white/10
+
+            bg-white/[0.02]
+
+            px-4
+            py-3
+
+            hover:bg-white/[0.04]
+            hover:cursor-pointer
+
+            transition"
       >
-        Filters
+          <div className="flex items-center gap-3">
+            <ListFilter size={18} />
+
+            <span className="font-medium">
+                Filters
+            </span>
+        </div>
       </button>
 
       <div
