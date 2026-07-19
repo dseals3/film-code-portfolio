67a06449162b89a9075cbc9e44d542d5ef988428
Damian Seals <46690930+dseals3@users.noreply.github.com>
2026-07-19 15:30:37 -0400

    moved client ops out of page so entire page wasnt on client

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,28 +1,7 @@
-"use client"
-
 import { work } from "../data/work";
-import WorkGrid from "../components/WorkGrid"
-import FilterSidebar from "../components/FilterSidebar";
-import MobileFilterWrapper from "../components/MobileFilterWrapper";
-import { useState } from "react";
+import WorkFilters from "../components/WorkFilters";
 
 export default function WorksPage() {
-  const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
-
-  const filteredProjects =
-  selectedTypes.length === 0
-    ? work
-    : work.filter((project) =>
-        selectedTypes.includes(project.type)
-      );
-
-  function toggleType(type: string) {
-    setSelectedTypes((prev) =>
-      prev.includes(type)
-        ? prev.filter((t) => t !== type)
-        : [...prev, type]
-    );
-  }
   return (
     <main className="min-h-screen bg-black text-white p-8 py-24">
       <div className="max-w-6xl mx-auto">
@@ -33,22 +12,7 @@ export default function WorksPage() {
         <p className="text-gray-400 mb-12">
           A complete archive of narrative, commercial, and development projects.
         </p>
-        <div className="grid grid-cols-1 lg:grid-cols-[220px_1fr] gap-12">
-
-          {/* Filters */}
-          <MobileFilterWrapper>
-            <FilterSidebar
-              selectedTypes={selectedTypes}
-              toggleType={toggleType}
-            />
-          </MobileFilterWrapper>
-          <WorkGrid
-              projects={filteredProjects}
-              columns={3}
-              showDescription
-              descriptionLines={2}
-          />
-        </div>
+        <WorkFilters projects={work} />
       </div>
     </main>
   );
