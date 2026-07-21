798ba369f74ccfdec9415244e5944d7d594ca9d8
Damian Seals <46690930+dseals3@users.noreply.github.com>
2026-07-20 20:30:41 -0400

    moved types out of component into this file

--- app/data/filterOptions.ts
+++ app/data/filterOptions.ts
@@ -1,21 +1,46 @@
-    export const toolFilters: Record<string, string[]> = {
-      "Sony Camera": [
-        "Sony a7iv",
-        "Sony ZV-E1",
-        "Sony FX3",
-        "Sony FX6"
-      ],
-      "Blackmagic Camera": [
-        "Blackmagic Pocket 6k Pro",
-      ],
-      "Drone": [
-        "DJI Mini 3 Pro"
-      ],
-      "Custom Rig": [
-        "Custom Drop Rig",
-        "Plexiglass Platform"
-      ],
-      "Davinci Resolve": [
-        "Davinci Resolve"
-      ]
-    };
+export const workTypes = [
+    "Narrative",
+    "Commercial",
+    "Music Video",
+    "Youtube",
+    "Vertical",
+    "Software",
+];
+export const roles = [
+    "Director",
+    "Director of Photography",
+    "1st Assistant Camera",
+    "Gaffer",
+    "Grip",
+    "Editor",
+    "Colorist",
+    "Production Assistant"
+];
+export const tools = [
+    "Sony Camera",
+    "Blackmagic Camera",
+    "Drone",
+    "Custom Rig",
+    "Davinci Resolve"
+]
+export const toolFilters: Record<string, string[]> = {
+    "Sony Camera": [
+    "Sony a7iv",
+    "Sony ZV-E1",
+    "Sony FX30",
+    "Sony FX6"
+    ],
+    "Blackmagic Camera": [
+    "Blackmagic Pocket 6k Pro",
+    ],
+    "Drone": [
+    "DJI Mini 3 Pro"
+    ],
+    "Custom Rig": [
+    "Custom Drop Rig",
+    "Plexiglass Platform"
+    ],
+    "Davinci Resolve": [
+    "Davinci Resolve"
+    ]
+};
