28cac6ca5a4f8a968303de4a2de9a7b57386aa4e
Damian Seals <46690930+dseals3@users.noreply.github.com>
2026-07-20 20:23:53 -0400

    added new toll filter groups

---
+++ app/data/filterOptions.ts
@@ -0,0 +1,21 @@
+    export const toolFilters: Record<string, string[]> = {
+      "Sony Camera": [
+        "Sony a7iv",
+        "Sony ZV-E1",
+        "Sony FX3",
+        "Sony FX6"
+      ],
+      "Blackmagic Camera": [
+        "Blackmagic Pocket 6k Pro",
+      ],
+      "Drone": [
+        "DJI Mini 3 Pro"
+      ],
+      "Custom Rig": [
+        "Custom Drop Rig",
+        "Plexiglass Platform"
+      ],
+      "Davinci Resolve": [
+        "Davinci Resolve"
+      ]
+    };
