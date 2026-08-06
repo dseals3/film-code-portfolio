655f4507cab394a2292beaa53537ec9d7e73974c
Damian Seals <46690930+dseals3@users.noreply.github.com>
2026-08-06 19:28:03 -0400

    rewording of about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -30,23 +30,26 @@ export default function About() {
 
             {/* TEXT */}
             <div className="md:w-2/3 space-y-6 text-gray-300 leading-relaxed">
-                <h2 className="text-2xl font-semibold mb-2 text-gray-200">Filmmaker</h2>
+                <h2 className="text-2xl font-semibold mb-2 text-gray-100">Filmmaker</h2>
                 <p className="mb-10">
                     I'm more than just a Cinematographer - One-man Youtube productions, Director of Photography, 1st or 2nd Assistant Camera, Editor, Colorist - I've done it all.<br /><br />
 
-                    It's 2009, I'm at home playing on my parents' laptop and I find an intriguing star-shaped logo on the dock. "What's that?" I wonder. Within 24 hours I had learned the basics, recorded, and edited the very first Youtube video.<br /><br />
+                    It all began in 2009: I was at home playing on my parents' laptop when I found an intriguing, star-shaped logo on the dock. "What's that?" I wonder, and within 24 hours I had learned the basics, recorded, and edited my very first Youtube video.<br /><br />
 
-                    From iMovie to After Effects, Premiere, and finally Davinci Resolve; from my first camcorder to a Canon G7x, my first Sony a7, and now Blackmagic and film cameras; and from making Youtube videos with my sister in the basement to DP'ing on real sets with a whole team - it all started that one morning when I opened iMovie for the very first time.<br /><br />
+                    I'm self-taught. From iMovie to After Effects, Premiere, and finally Davinci Resolve; from a laptop webcam to a Canon G7x, my first Sony a7, and now cinema and film cameras; and from making Youtube videos with my sister in the basement to DP'ing on real sets with a whole team - it all started that morning when I opened iMovie for the first time.<br /><br />
 
-                    One thing remains consistent, however: to improve and learn with each project - to tell the story more engagingly, to present visuals more stunningly, and/or to help a team of people do the same.
+                    I have strong roots in Youtube - for many years I've watched Youtube just to learn how to make better videos for Youtube - which has allowed me to find my style and supercharge my work in film. I'm drawn to movement, high-contrast imagery, and engaging edits that evoke a feeling and matches a story's pace.<br /><br />
+
+                    One thing remains consistent, however: to improve and learn with each project - to tell the story more engagingly, to present visuals more stunningly, and/or to help a team of people do the same.<br /><br />
+
                 </p>
 
-                <h2 className="text-2xl font-semibold mb-2 text-gray-200">Software Engineer</h2>
+                <h2 className="text-2xl font-semibold mb-2 text-gray-100">Software Engineer</h2>
                 <p className="mb-10">
                     I'm also a software engineer with a Masters Degree in Computer Science and experience spanning Full-Stack web development, databases, interactive applications, and reporting.<br /><br />
                     
                     At CodeLevel Services, I led reporting and data visualization initiatives, helping transform business data into actionable insights for clients. This experience strengthened my ability to work with databases, analyze complex datasets, and deliver aesthetically pleasing reporting solutions that support real-world decision-making.<br /><br />
-                    During my internship at Discovery, Inc., I contributed to an internal web application using Python, Flask, SQL, and HTML/CSS. In addition to application development, I gained exposure to DevOps workflows, automation, and database-driven systems within a large-sccale enterprise environement.<br /><br />
+                    During my internship at Discovery, Inc., I contributed to an internal web application using Python, Flask, SQL, and HTML/CSS. In addition to application development, I gained exposure to DevOps workflows, automation, and database-driven systems within a large-scale enterprise environement.<br /><br />
                     At the University of Tennessee, I served as both a Teaching Assistant, where I taught fellow students and graded coursework, and as a Research Assistant, where I used Unreal Engine 5 to help create an interactive Virtual Reality training experience in pursuit of proper Alzheimer's Patient Care<br /><br />
                     
                     Beyond my professional work, I also built this very website as a personal project using Next.js, React, TypeScript, and Tailwind CSS.
